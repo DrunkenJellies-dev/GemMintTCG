f7ceb7764ddac1ec6a982bc7e670aa9cf0f605fd
CallumNewman <ballumnew@gmail.com>
2025-01-29 22:49:47 +0000

    Added the initial home page

--- GemMintTCG/settings.py
+++ GemMintTCG/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'home',
 
     # The following apps are required by django-allauth:
     'allauth',
@@ -61,7 +63,10 @@ ROOT_URLCONF = 'GemMintTCG.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
