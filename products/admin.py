fa2ce80845c1a1d4006634078b8224fd1fe6c7f2
CallumNewman <ballumnew@gmail.com>
2025-02-02 04:52:46 +0000

    completed multiple image support for products

--- products/admin.py
+++ products/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
-from .models import Product, Category
+from .models import Product, Category, ProductImage
 
-# Register your models here.
+class ProductImageInline(admin.TabularInline):  
+    model = ProductImage
+    extra = 1 
+    fields = ('image', 'alt_text')  
+
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('sku', 'name', 'get_categories', 'price', 'rating', 'image',)
+    list_display = ('sku', 'name', 'get_categories', 'price', 'rating',)
     filter_horizontal = ('categories',)
     ordering = ('sku',)
+    inlines = [ProductImageInline]  
 
     def get_categories(self, obj):
         return ", ".join([category.friendly_name or category.name for category in obj.categories.all()])
--- products/models.py
+++ products/models.py
@@ -24,7 +24,14 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, related_name='images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='product_images/')
+    alt_text = models.CharField(max_length=255, null=True, blank=True)
+
+    def __str__(self):
+        return f"Image for {self.product.name}"
