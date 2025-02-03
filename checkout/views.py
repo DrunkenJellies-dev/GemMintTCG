24963231eea91a12ac08808abc28667cc0df4eb5
CallumNewman <ballumnew@gmail.com>
2025-02-03 02:05:36 +0000

    added basic checkout functionality with stripe

--- checkout/views.py
+++ checkout/views.py
@@ -9,6 +9,9 @@ import stripe
 
 # Create your views here.
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
 
     if not bag:
@@ -18,13 +21,24 @@ def checkout(request):
     current_bag = bag_contents(request)
     total = current_bag['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+    
+    print(intent)
 
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form':order_form,
-        'stripe_public_key': 'pk_test_51QoEJqCa0DZAJIS6xWdKl8Ryb3MUR9nHk4n6mpwPsCDseZaWfqk5aGUzWJLeNIoap3R9j2OD3N2eRX1vQHavunmf00dEEicd08',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
