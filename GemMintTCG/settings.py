c9d898126b6aa84f986406569df883e49f5f0424
CallumNewman <ballumnew@gmail.com>
2025-02-03 23:32:27 +0000

    updated database login in settings

--- GemMintTCG/settings.py
+++ GemMintTCG/settings.py
@@ -129,15 +129,16 @@ WSGI_APPLICATION = 'GemMintTCG.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if DEBUG:
+if 'DATABASE_URL' in os.environ:
+    DATABASES = { 'default': dj_database_url.parse(os.getenv('DATABASE_URL', '')) }
+else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
-else:
-    DATABASES = { 'default': dj_database_url.parse(os.getenv('DATABASE_URL', '')) }
+    
 
 
 # Password validation
