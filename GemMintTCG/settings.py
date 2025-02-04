7ba1139611c6214d4f41b4a67a0ab1de322060f0
CallumNewman <ballumnew@gmail.com>
2025-02-04 00:25:52 +0000

    modified settings.py for release

--- GemMintTCG/settings.py
+++ GemMintTCG/settings.py
@@ -11,12 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
-from dotenv import load_dotenv
 from pathlib import Path
 import dj_database_url
-
-# Load environment variables from .env file
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,12 +126,14 @@ WSGI_APPLICATION = 'GemMintTCG.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 if 'DATABASE_URL' in os.environ:
-    DATABASES = { 'default': dj_database_url.parse(os.getenv('DATABASE_URL', '')) }
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
 else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
     
