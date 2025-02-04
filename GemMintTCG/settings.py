1db168fe44e55bc6e65f4b6845106c3fc44603c4
CallumNewman <ballumnew@gmail.com>
2025-02-04 07:11:00 +0000

    Corrected changes

--- GemMintTCG/settings.py
+++ GemMintTCG/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    "django.contrib.sitemaps",
 
     # Created Apps
     'home',
